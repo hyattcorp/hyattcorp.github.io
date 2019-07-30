506170d35fb8b5e7905cbf8523be0608ea962193
hyattcorp <48832185+hyattcorp@users.noreply.github.com>
2019-07-30 21:38:57 +0100

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -39,10 +39,10 @@ $('html, body').animate({scrollTop:scrollto}, 0);
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var hash = this.hash;
           var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          var target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html,body').animate({
-              scrollTop: target.offset().top -250
+              scrollTop: target.offset().top
             }, 1000, function () {
                 location.hash = hash;
             });
